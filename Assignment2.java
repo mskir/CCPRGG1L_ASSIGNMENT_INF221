f196fc9333799a2122cd5f9a9383029c3db96804
Journey Gamas <111472903+mskir@users.noreply.github.com>
2022-10-03 11:35:01 +0800

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,41 +1,33 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    
-    int rideCount = 5;
-    
-    // FOR LOOP
-    for (int i = 5; i != 0; i--) {
-        if (i == 4) {
-            break;
+
+        // FOR LOOP
+        for (int firstNameCount = 5; firstNameCount != 0; firstNameCount--) {
+            if (firstNameCount == 4) {
+                break;
+            }
+
+            System.out.println("Journey");
         }
-        
-        System.out.println("Journey");
-    }
-        
-        
-    // DO WHILE
-    int surname = 5;
-    
-    do {
-        System.out.println("Gamas");
-        surname--;
-        } while (surname != 0);
-        
-        
-    // WHILE LOOP
-    int nickname = 5;
-    while (nickname != 0) {
-        nickname--;
-        
-        if (nickname == 3) {
+
+        // DO WHILE
+        int surNameCount = 5;
+
+        do {
+            System.out.println("Gamas");
+            surNameCount--;
+        } while (surNameCount != 0);
+
+        // WHILE LOOP
+        int nickNameCount = 5;
+        while (nickNameCount != 0){
+            
+            nickNameCount--;
+            if (nickNameCount == 3) {
+                continue;
+            }
             System.out.println("Journ");
-            continue;
+           
         }
-        
-        System.out.println("J!");
     }
 }
-}
-
-
-
